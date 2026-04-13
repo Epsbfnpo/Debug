fd4ae76659a465e77f8cdaaa728573fb3e7fd2fd
Zhenlin Liu <84002104+Epsbfnpo@users.noreply.github.com>
2026-04-14 04:12:14 +1000

    Simplify data_manager to photometric-only transforms

--- dataset/data_manager.py
+++ dataset/data_manager.py
@@ -7,16 +7,6 @@ import torch
 import numpy as np
 from PIL import Image
 
-class SquarePad:
-    def __call__(self, image):
-        w, h = image.size
-        max_wh = max(w, h)
-        p_left = (max_wh - w) // 2
-        p_top = (max_wh - h) // 2
-        p_right = max_wh - w - p_left
-        p_bottom = max_wh - h - p_top
-        return F.pad(image, (p_left, p_top, p_right, p_bottom), 0, 'constant')
-
 def get_dataset(args, cfg):
     if cfg.ALGORITHM != 'GDRNet' and cfg.ALGORITHM != 'CASS_GDRNet':
         train_ts, test_ts, tra_fundus = get_transform(cfg)
@@ -45,12 +35,11 @@ def get_dataset(args, cfg):
     return train_loader, val_loader, test_loader, dataset_size, train_sampler
 
 def get_transform(cfg):
-    size = 512
     re_size = 512
     normalize = get_normalize()
-    tra_train = transforms.Compose([SquarePad(), transforms.Resize((size, size)), transforms.RandomResizedCrop(re_size, scale=(0.7, 1.0)), transforms.RandomHorizontalFlip(), transforms.ColorJitter(0.3, 0.3, 0.3, 0.3), transforms.RandomGrayscale(), transforms.ToTensor(), normalize])
-    tra_test = transforms.Compose([SquarePad(), transforms.Resize((size, size)), transforms.Resize((re_size, re_size)), transforms.ToTensor(), normalize])
-    tra_mask = transforms.Compose([SquarePad(), transforms.Resize((re_size, re_size)), transforms.ToTensor()])
+    tra_train = transforms.Compose([transforms.RandomResizedCrop(re_size, scale=(0.7, 1.0)), transforms.RandomHorizontalFlip(), transforms.ColorJitter(0.3, 0.3, 0.3, 0.3), transforms.RandomGrayscale(), transforms.ToTensor(), normalize])
+    tra_test = transforms.Compose([transforms.ToTensor(), normalize])
+    tra_mask = transforms.Compose([transforms.ToTensor()])
     return tra_train, tra_test, tra_mask
 
 def get_pre_FundusAug(cfg):
@@ -58,12 +47,10 @@ def get_pre_FundusAug(cfg):
     jitter_c = getattr(cfg.TRANSFORM, 'COLORJITTER_C', 0.2)
     jitter_s = getattr(cfg.TRANSFORM, 'COLORJITTER_S', 0.2)
     jitter_h = getattr(cfg.TRANSFORM, 'COLORJITTER_H', 0.1)
-    size = 512
-    re_size = 512
     normalize = get_normalize()
-    tra_train = transforms.Compose([SquarePad(), transforms.Resize((size, size)), transforms.ColorJitter(brightness=jitter_b, contrast=jitter_c, saturation=jitter_s, hue=jitter_h), transforms.ToTensor()])
-    tra_test = transforms.Compose([SquarePad(), transforms.Resize((size, size)), transforms.CenterCrop(re_size), transforms.ToTensor(), normalize])
-    tra_mask = transforms.Compose([SquarePad(), transforms.Resize((size, size), interpolation=transforms.InterpolationMode.NEAREST), transforms.ToTensor()])
+    tra_train = transforms.Compose([transforms.ColorJitter(brightness=jitter_b, contrast=jitter_c, saturation=jitter_s, hue=jitter_h), transforms.ToTensor()])
+    tra_test = transforms.Compose([transforms.ToTensor(), normalize])
+    tra_mask = transforms.Compose([transforms.ToTensor()])
     return tra_train, tra_test, tra_mask
 
 def get_post_FundusAug(cfg):
